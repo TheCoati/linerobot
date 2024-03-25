a7ab4e6259a3a9225108a11afb0f0f19f3e34e2e
TheCoati <hello@thecoati.dev>
2024-03-25 15:56:13 +0100

    feat: first release!

--- linefollow.ino
+++ linefollow.ino
@@ -21,23 +21,25 @@ const int DISPLAY_SEGMENT_PINS[7] = { 0, 1, 2, 4, 5, 6, 7 };
 const int DISPLAY_OFF[7] = { LOW, LOW, LOW, LOW, LOW, LOW, LOW };
 const int LETTER_S[7] = { HIGH, LOW, HIGH, HIGH, LOW,  HIGH, HIGH };
 const int LETTER_T[7] = { LOW,  LOW, LOW,  HIGH, HIGH, HIGH, HIGH };
+const int LETTER_F[7] = { HIGH, LOW, LOW,  LOW,  HIGH, HIGH, HIGH };
+const int LETTER_I[7] = { LOW,  LOW, LOW,  LOW, HIGH, HIGH,  LOW  };
 
 // Display numbers
 const int NUMBERS[10][7] = {
-    { HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH }, // 0
-    { HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH }, // 1
-    { HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH }, // 2
-    { HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH }, // 3
-    { HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH }, // 4
-    { HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH }, // 5
-    { HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH }, // 6
-    { HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH }, // 7
+    { HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, LOW  }, // 0
+    { LOW,  HIGH, HIGH, LOW,  LOW,  LOW,  LOW  }, // 1
+    { HIGH, HIGH, LOW,  HIGH, HIGH, LOW,  HIGH }, // 2
+    { HIGH, HIGH, HIGH, HIGH, LOW,  LOW,  HIGH }, // 3
+    { LOW,  HIGH, HIGH, LOW,  LOW,  HIGH, HIGH }, // 4
+    { HIGH, LOW,  HIGH, HIGH, LOW,  HIGH, HIGH }, // 5
+    { HIGH, LOW,  HIGH, HIGH, HIGH, HIGH, HIGH }, // 6
+    { HIGH, HIGH, HIGH, LOW,  LOW,  LOW,  LOW  }, // 7
     { HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH }, // 8
-    { HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH }, // 9
+    { HIGH, HIGH, HIGH, HIGH, LOW,  HIGH, HIGH }, // 9
 };
 
 // Configuration
-const int FINISH_DETECTED_DELAY = 1000;       // How long the sensor should detect full black to trigger finish.
+const int FINISH_DETECTED_DELAY = 200;       // How long the sensor should detect full black to trigger finish.
 const int DEFAULT_LEFT_SPEED = 75;           // Default left engine speed in forward motion.
 const int DEFAULT_RIGHT_SPEED = 60;          // Default right engine speed in forward motion.
 const int TURN_SPEED = 80;                   // Speed while turning.
@@ -45,6 +47,7 @@ const int ADJUST_SPEED = 40;                 // Speed while adjusting.
 const int ROTATE_SPEED = 50;                 // Speed while turning around on both engine.
 const int ROTATE_ON_OBJECT_DISTANCE = 8.0;   // Object distance to trigger turn around.
 const int DISPLAY_MULTIPLEX_DELAY = 1;       // Delay between displays for multiplexing.
+const int TURN_AROUND_ON_OBJECT_DELAY = 500; 
 
 enum Sensors
 {
@@ -57,17 +60,24 @@ enum Sensors
 
 UltraSonicDistanceSensor distanceSensor(TRIG_PIN, ECHO_PIN);
 
-bool started = false;
+bool running = false;
+bool finished = false;
+bool countUp = false;
+bool showEndTime = false;
+int showEndTimeCount = 0;
 
 unsigned long now = millis();
 unsigned long finishDetectedTime = 0;
 unsigned long lastDisplayUpdate = 0;
 unsigned long lastPingSensorUpdate = 0;
+unsigned long lastCountdownUpdate = 0;
+unsigned long forceRotate = 0;
 
 bool hasDetectedObject = false;
 
 int currentDisplay = 0;
 int* display[2] = { DISPLAY_OFF, DISPLAY_OFF };
+int timer = 10;
 
 /**
  * Show value on the displays.
@@ -75,6 +85,11 @@ int* display[2] = { DISPLAY_OFF, DISPLAY_OFF };
  */
 void renderDisplay()
 {
+    // Do not tick timer if countdown has not been started
+    if (lastCountdownUpdate > 0) {
+        tickTimer();
+    }
+
     unsigned long timePassed = now - lastDisplayUpdate;
     int commonPin = DISPLAY_PINS[currentDisplay];
     int size = sizeof(display) / sizeof(display[0]);
@@ -90,13 +105,15 @@ void renderDisplay()
 
     digitalWrite(commonPin, HIGH);
 
-    // Switch display (Multiplex)
-    if (timePassed >= DISPLAY_MULTIPLEX_DELAY && size > 1) {
-        currentDisplay = (currentDisplay == 0 ? 1 : 0);
-        lastDisplayUpdate = millis();
+    if (size > 1) {
+        // Switch display (Multiplex)
+        if (timePassed >= DISPLAY_MULTIPLEX_DELAY && size > 1) {
+            currentDisplay = (currentDisplay == 0 ? 1 : 0);
+            lastDisplayUpdate = millis();
+        }
     } else {
         currentDisplay = 0;
-    }
+    } 
 }
 
 /**
@@ -110,12 +127,71 @@ void setDisplay(int display1[7], int display2[7])
     display[1] = display2 == NULL ? DISPLAY_OFF : display2;    
 }
 
+void setDisplayNumber(int value) {
+    if (value < 10) {
+        setDisplay(NUMBERS[value], NULL);
+    } else {
+        setDisplay(NUMBERS[value / 10], NUMBERS[value % 10]);
+    }
+}
+
 /**
  * Start the countdown to start the robot.
  */
 void startCountdown()
 {
+    setDisplayNumber(timer);
+    lastCountdownUpdate = millis();
+}
 
+void tickTimer()
+{
+    if (showEndTimeCount == 4) {
+        setDisplay(LETTER_F, LETTER_I);
+        return;
+    }
+
+    unsigned long timePassed = now - lastCountdownUpdate;
+    int interval = finished ? 500 : 1000;
+
+    if (timePassed >= interval) {
+        if (finished) {
+            if (showEndTime) {
+                setDisplayNumber(timer);
+                showEndTimeCount++;
+            } else {
+                setDisplay(DISPLAY_OFF, DISPLAY_OFF);
+            }
+            
+            showEndTime = !showEndTime;
+            lastCountdownUpdate = millis();
+            return;
+        }
+
+        if (countUp) {
+            timer++;
+
+            if (timer == 1) {
+                running = true;
+            }
+
+            if (timer == 100) {
+                timer = 1;
+            }
+        } else {
+            timer--;
+
+            if (timer == 0) {
+                countUp = true;
+                setDisplay(LETTER_S, LETTER_T);
+                lastCountdownUpdate = millis();
+                return;
+            }
+        }
+
+        setDisplayNumber(timer);
+        lastCountdownUpdate = millis();
+    }
 }
 
 /**
@@ -131,9 +207,12 @@ bool detectObject()
         double distance = distanceSensor.measureDistanceCm();
 
         hasDetectedObject = distance <= ROTATE_ON_OBJECT_DISTANCE && distance > 0;
-    }
 
-    return hasDetectedObject;
+        if (hasDetectedObject) {
+            forceRotate = now + TURN_AROUND_ON_OBJECT_DELAY;
+            turnAround();
+        }
+    }
 }
 
 /**
@@ -219,9 +298,16 @@ void turnAround()
 /**
  * Stop the robot.
  */
-void stop() {
+void stop()
+{
     analogWrite(RIGHT_PWM_PIN, 0);
     analogWrite(LEFT_PWM_PIN, 0);
+}
+
+void finish() {
+    running = false;
+    finished = true;
+    stop();
 }
 
 /**
@@ -230,6 +316,8 @@ void stop() {
  */
 bool checkFinish()
 {
+    turnRight();
+
     if (finishDetectedTime == 0) {
         finishDetectedTime = millis();
         return;
@@ -238,14 +326,11 @@ bool checkFinish()
     unsigned long timePassed = now - finishDetectedTime;
 
     if (timePassed >= FINISH_DETECTED_DELAY) {
-        stop();
+        finish();
     };
 }
 
-
-
 void setup()
-
 {
     Serial.end(); // Disable Serial
     // Serial.begin(9600);
@@ -278,9 +363,6 @@ void setup()
     pinMode(ECHO_PIN, INPUT);
     pinMode(TRIG_PIN, OUTPUT);
     digitalWrite(TRIG_PIN, LOW);
-
-    // Default display value St.
-    setDisplay(LETTER_S, LETTER_T);
 }
 
 void loop()
@@ -290,21 +372,31 @@ void loop()
     // Displays
     renderDisplay();
 
-    // Ping sensor
-    if (detectObject()) {
-        turnAround();
-        delay(500);
+    // Line sensor
+    int sensor1 = digitalRead(SENSOR_PINS[0]) == LOW ? SENSOR_1 : 0;
+    int sensor2 = digitalRead(SENSOR_PINS[1]) == LOW ? SENSOR_2 : 0;
+    int sensor3 = digitalRead(SENSOR_PINS[2]) == LOW ? SENSOR_3 : 0;
+    int sensor4 = digitalRead(SENSOR_PINS[3]) == LOW ? SENSOR_4 : 0;
+    int sensor5 = digitalRead(SENSOR_PINS[4]) == LOW ? SENSOR_5 : 0;
+  
+    const int state = (sensor1 | sensor2 | sensor3 | sensor4 | sensor5);
+
+    // Start countdown when put on line
+    if (lastCountdownUpdate == 0 && state == SENSOR_3) {
+        startCountdown();
+    }
+
+    if (!running) {
         return;
     }
 
-   // Line sensor
-   int sensor1 = digitalRead(SENSOR_PINS[0]) == LOW ? SENSOR_1 : 0;
-   int sensor2 = digitalRead(SENSOR_PINS[1]) == LOW ? SENSOR_2 : 0;
-   int sensor3 = digitalRead(SENSOR_PINS[2]) == LOW ? SENSOR_3 : 0;
-   int sensor4 = digitalRead(SENSOR_PINS[3]) == LOW ? SENSOR_4 : 0;
-   int sensor5 = digitalRead(SENSOR_PINS[4]) == LOW ? SENSOR_5 : 0;
+    // Ping sensor
+    detectObject();
 
-   const int state = (sensor1 | sensor2 | sensor3 | sensor4 | sensor5);
+    if (now < forceRotate) {
+        turnAround();
+        return;
+    }
 
     // If state was not detecting the finish reset the finish detection timer
     if (state != (SENSOR_1 | SENSOR_2 | SENSOR_3 | SENSOR_4 | SENSOR_5)) {
@@ -318,11 +410,13 @@ void loop()
         case (SENSOR_3):
             moveForward();
             break;
+        case (SENSOR_1 | SENSOR_2 | SENSOR_3 | SENSOR_4):   
         case (SENSOR_1 | SENSOR_2 | SENSOR_3):   
         case (SENSOR_1 | SENSOR_2): 
         case (SENSOR_1):
             turnLeft();
             break;
+        case (SENSOR_2 | SENSOR_3 | SENSOR_4 | SENSOR_5):    
         case (SENSOR_3 | SENSOR_4 | SENSOR_5):
         case (SENSOR_4 | SENSOR_5):    
         case (SENSOR_5):
